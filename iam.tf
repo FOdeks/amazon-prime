0e6b404f9602fb8bafa4cc13ddfb2706e2985ff7
FOdeks <femi.odekunle@outlook.com>
2022-08-17 21:54:59 -0400

    new iam changes

---
+++ iam.tf
@@ -0,0 +1,10 @@
+kdfgjsdfl;gksdf'gksd
+fgssdvgsdsgdfglocals {
+  h';sfjsdhfjdshflkfj
+  gh';dfkgdf
+  kghdnksdk;lfjd;fjwse;
+  f'hlnkbn;sdfljkgpouwjvs;lfj
+  hltrghly
+  t']jk;sndgjkhgwerjihfgoiwerhj
+  uysdhfkjagkdaejgr;jg
+}
